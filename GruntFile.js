91d0a2f6cd98eabcb0d92cc790041e5233d65bf4
Muhanad <neo0rabie@gmail.com>
2015-05-17 23:31:21 -0700

    html and css files minified and project is ready for submission

--- GruntFile.js
+++ GruntFile.js
@@ -11,7 +11,35 @@ module.exports = function(grunt){
         }]
       }
     },
+    htmlmin: {                                     
+      dist: {                                      
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'index.html': 'index.html',
+          'project-2048.html': 'project-2048.html',
+          'project-mobile.html': 'project-mobile.html', 
+          'project-webperf.html': 'project-webperf.html' // 'destination': 'source'
+        }
+      }
+    },
+    cssmin: {
+       minify: {
+         options: {
+           banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+         },
+         expand: true,
+         src: ['*.css', '!*.min.css'],
+         dest: 'dist/',
+         ext: '.min.css'
+       }
+     },
   });
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.registerTask('default', ['imagemin']);
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['htmlmin']);
+  // grunt.registerTask('default', ['imagemin']);
 }
